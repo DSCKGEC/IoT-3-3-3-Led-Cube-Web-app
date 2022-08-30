42c0083329b43b88a6d08e0c95bdf746ba88cf28
SohamDasBiswas <sohamdb123@gmail.com>
2022-08-30 15:20:57 +0530

    light/dark mode switch done

---
+++ static/js/app.js
@@ -0,0 +1,6 @@
+let lightDark =document.getElementById('toggle2_checkbox')
+
+lightDark.onclick = function(){
+    document.body.classList.toggle("dark-theme")
+    document.body.classList.toggle("on-off-Switch-dark")
+}
